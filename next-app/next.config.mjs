14f36e91be77834e006bff3bd223924fd559b06e
Dhirajsingh212 <dhirajsingh0229@gmail.com>
2024-10-09 10:48:37 +0530

    minor updates

--- next-app/next.config.mjs
+++ next-app/next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  compiler: {
-    removeConsole: process.env.NODE_ENV === "production" ? true : false,
-  },
   images: {
     remotePatterns: [
       { hostname: "res.cloudinary.com" },
